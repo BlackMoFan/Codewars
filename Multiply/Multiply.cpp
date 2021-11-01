c3ace7344c57fe1f313991d22bb339ab4f3f4287
BlackMoFan <rodlester.moreno23@gmail.com>
2021-11-02 00:40:07 +0800

    Added Multiply challenge in Codewars

---
+++ Multiply/Multiply.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int multiply(int a, int b){
+  return a*b;
+}
+int main(){
+  std::cout << multiply(42,20) << std::endl;
+  return 0;
+}
---
+++ Multiply/Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply{
+  public static double multiply(double a, double b){
+    return a*b;
+  }
+
+  public static void main(String[] args){
+    System.out.println(Multiply(2.5,3.0));
+  }
+}
